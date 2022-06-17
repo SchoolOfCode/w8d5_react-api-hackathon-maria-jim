20ed76b4282a81720a28cc5083863e83e6516caa
maroaraujo <tiagomoller@tiagos-mbp.home>
2022-06-17 15:25:27 +0100

    Button is working

--- cocktail-app/src/components/App/App.js
+++ cocktail-app/src/components/App/App.js
@@ -4,6 +4,7 @@ import Cocktail from "../Cocktail";
 import CocktailImage from "../CocktailImage";
 import IngredientsUL from "../IngredientsUL";
 import IngredientsLI from "../IngredientsLI";
+import ButtonNewRecipe from "../Button";
 
 function App() {
   const [pictureURL, setPictureURL] = useState("");
@@ -18,6 +19,7 @@ function App() {
     );
     console.log(response);
     let data = await response.json();
+
     let arrIngredients = [];
     let arrMeasures = [];
 
@@ -50,9 +52,48 @@ function App() {
     getRandomRecipe();
   }, []);
 
+  const buttonName = "Get new recipe!";
+
+  function handleClick(){
+    console.log("handleclick working")
+    async function getNewRandomRecipe() {
+      let response = await fetch(
+        "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+      );
+      console.log(response);
+      let data = await response.json();
+  
+      let arrIngredients = [];
+      let arrMeasures = [];
+  
+      setPictureURL(data.drinks[0].strDrinkThumb);
+      setCocktailName(data.drinks[0].strDrink);
+      setCocktailInstructions(data.drinks[0].strInstructions);
+  
+      for (let i = 1; i < 16; i++) {
+        const key1 = "strIngredient" + i;
+        const key2 = "strMeasure" + i;
+        const ingredient = data.drinks[0][key1];
+        const measure = data.drinks[0][key2];
+        if (ingredient !== null) {
+          arrIngredients.push(ingredient);
+          arrMeasures.push(measure);
+        }
+      }
+      setCocktailIngredients(arrIngredients);
+      setCocktailMeasures(arrMeasures);
+  
+      //console.log(data);
+      console.log("our Ingreds", arrIngredients);
+      console.log("our Measures", arrMeasures);
+    }
+    getNewRandomRecipe();
+  }
+
   return (
     <div className="App">
       <h1>Cocktail Inspirations</h1>
+      <div><ButtonNewRecipe text={buttonName} onClick ={handleClick}/></div>
       <div id="recipe-div">
         <Cocktail text={cocktailName} />
         <CocktailImage src={pictureURL} text={cocktailName} />
